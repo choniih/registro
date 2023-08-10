138a6fe2821ef32b0f7ea5b731439509bf951553
choniih <n.heguaburo@gmail.com>
2023-08-10 19:30:10 -0300

    funciones

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,29 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+const regBtn = document.getElementById("regBtn");
+regBtn.addEventListener("click", validar);
+
+function validar() {
+
+    const check = document.getElementById("terminos").checked;
+    const pass1 = document.getElementById("password1").value;
+    const pass2 = document.getElementById("password2").value;
+    const mail = document.getElementById("email").value;
+    const apellido = document.getElementById("apellido").value;
+    const nombre = document.getElementById("nombre").value;
+
+    if (nombre !== "" && apellido !== "" && mail !== "" && pass1 !== "" && check) {
+        if (pass1 === pass2 && pass1.length > 5) {
+            showAlertSuccess();
+        } else {
+            showAlertError();
+        }
+    } else {
+        showAlertError();
+    }
+
+    
+}
